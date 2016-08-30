f7294ed505265a355d75b698ff77cc7233ab36e2
ppli2015 <lipeipei2014@gmail.com>
2016-08-30 09:18:37 +0800

    update files

---
+++ 13 Roman to Integer.py
@@ -0,0 +1,24 @@
+# -*-coding:cp936-*-
+__author__ = 'lpp'
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        for i in range(len(s) - 1):
+            if dic[s[i]] >= dic[s[i + 1]]:
+                result += dic[s[i]]
+            else:
+                result -= dic[s[i]]
+        result += dic[s[-1]]
+        return result
+
+
+test = Solution()
+r = test.romanToInt('MLXVI')
+print r
